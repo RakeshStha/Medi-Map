2be3eabe77f09e32e08da189b961eb4a944e1f4d
Aawveii-dngl <dangolavi9999@gmail.com>
2021-05-20 18:00:24 +0545

    My place added

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 27.597256056882415, lng: 85.31948317008779},
-        zoom: 17
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 27.597256056882415, lng: 85.31948317008779 },
+    zoom: 17,
+  });
+
+  //call marker function
+  addMarker({ lat: 27.597256056882415, lng: 85.31948317008779 });
+  addMarker({ lat: 27.5976411567244, lng: 85.32435651634849 });
+  addMarker({ lat: 27.600731889166706, lng: 85.31349128502795 });
+  addMarker({ lat: 27.599277175518917, lng: 85.31882351620831 });
+  addMarker({ lat: 27.641913, lng: 85.304522 });
+
+  //add marker function
+  function addMarker(coords) {
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: map,
+      icon: "./img/Logo.png",
     });
-
-    //call marker function
-    addMarker({lat:27.597256056882415,lng:85.31948317008779});
-    addMarker({lat:27.5976411567244,lng:85.32435651634849});
-    addMarker({lat:27.600731889166706,lng:85.31349128502795});
-    addMarker({lat:27.599277175518917,lng:85.31882351620831});
-   
-
-    //add marker function
-    function addMarker(coords){
-        var marker = new google.maps.Marker({
-            position:coords,
-            map:map,
-            icon:'./img/Logo.png',
-        });
-    }
+  }
 }
